0da82141466488fd2cdd6191ff20ebedbc225708
Alexis Serapio <alexis.serapioh@gmail.com>
2025-08-22 14:31:26 -0600

    Initial Commit

---
+++ Contalana_Prototipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contalana_Prototipe
+//
+//  Created by UNAM on 22/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
